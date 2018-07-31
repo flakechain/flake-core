ab55cc4c2c499b5c3b62225708c367c65ad29b48
0xA01 <cryptoape@icloud.com>
2018-07-31 05:11:55 +0300

    FlakeChain Config File

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -33,6 +33,8 @@
 #include <string>
 #include <boost/uuid/uuid.hpp>
 
+#include <flakechain.h>
+
 #define CRYPTONOTE_DNS_TIMEOUT_MS                       20000
 
 #define CRYPTONOTE_MAX_BLOCK_NUMBER                     500000000
@@ -41,18 +43,18 @@
 #define CRYPTONOTE_MAX_TX_SIZE                          1000000000
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60
-#define CURRENT_TRANSACTION_VERSION                     2
-#define CURRENT_BLOCK_MAJOR_VERSION                     1
-#define CURRENT_BLOCK_MINOR_VERSION                     0
+#define CURRENT_TRANSACTION_VERSION                     FLAKE_TRANSACTION_VERSION
+#define CURRENT_BLOCK_MAJOR_VERSION                     FLAKE_BLOCK_MAJOR_VERSION
+#define CURRENT_BLOCK_MINOR_VERSION                     FLAKE_BLOCK_MINOR_VERSION
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              60*60*2
 #define CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE             10
 
 #define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW               60
 
 // MONEY_SUPPLY - total number coins to be generated
-#define MONEY_SUPPLY                                    ((uint64_t)(-1))
+#define MONEY_SUPPLY                                    ((uint64_t)FLAKE_MONEY_SUPPLY)
 #define EMISSION_SPEED_FACTOR_PER_MINUTE                (20)
-#define FINAL_SUBSIDY_PER_MINUTE                        ((uint64_t)300000000000) // 3 * pow(10, 11)
+#define FINAL_SUBSIDY_PER_MINUTE                        ((uint64_t)FLAKE_FINAL_SUBSIDY_PER_MINUTE) // 3 * pow(10, 11)
 
 #define CRYPTONOTE_REWARD_BLOCKS_WINDOW                 100
 #define CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2    60000 //size of block (bytes) after which reward for block calculated using block size
@@ -117,11 +119,11 @@
 #define P2P_IDLE_CONNECTION_KILL_INTERVAL               (5*60) //5 minutes
 
 #define P2P_SUPPORT_FLAG_FLUFFY_BLOCKS                  0x01
-#define P2P_SUPPORT_FLAGS                               P2P_SUPPORT_FLAG_FLUFFY_BLOCKS
+#define P2P_SUPPORT_FLAGS                               P2P_SUPPORT_FLAG_FLUFFY_BLOCKS | P2P_SUPPORT_FLAGS_TROUBLESOME_BLOCKS
 
 #define ALLOW_DEBUG_COMMANDS
 
-#define CRYPTONOTE_NAME                         "bitmonero"
+#define CRYPTONOTE_NAME                         FLAKE_NAME
 #define CRYPTONOTE_POOLDATA_FILENAME            "poolstate.bin"
 #define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "data.mdb"
 #define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
@@ -148,19 +150,17 @@ namespace config
   uint8_t const FEE_CALCULATION_MAX_RETRIES = 10;
   uint64_t const DEFAULT_DUST_THRESHOLD = ((uint64_t)2000000000); // 2 * pow(10, 9)
   uint64_t const BASE_REWARD_CLAMP_THRESHOLD = ((uint64_t)100000000); // pow(10, 8)
-  std::string const P2P_REMOTE_DEBUG_TRUSTED_PUB_KEY = "0000000000000000000000000000000000000000000000000000000000000000";
+  std::string const P2P_REMOTE_DEBUG_TRUSTED_PUB_KEY = FLAKE_TRUSTED_PUBKEY;
 
-  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 18;
-  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 19;
-  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 42;
-  uint16_t const P2P_DEFAULT_PORT = 18080;
-  uint16_t const RPC_DEFAULT_PORT = 18081;
-  uint16_t const ZMQ_RPC_DEFAULT_PORT = 18082;
-  boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
-    } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-  uint32_t const GENESIS_NONCE = 10000;
+  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = FLAKE_PUBLIC_ADDRESS_BASE85_PREFIX;
+  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = FLAKE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX;
+  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = FLAKE_PUBLIC_SUBADDRESS_BASE58_PREFIX;
+  uint16_t const P2P_DEFAULT_PORT = FLAKE_P2P_DEFAULT_PORT;
+  uint16_t const RPC_DEFAULT_PORT = FLAKE_RPC_DEFAULT_PORT;
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = FLAKE_ZMQ_RPC_DEFAULT_PORT;
+  boost::uuids::uuid const NETWORK_ID = { FLAKE_NETWORK_ID }; // Bender's nightmare
+  std::string const GENESIS_TX = FLAKE_GENESIS_TX;
+  uint32_t const GENESIS_NONCE = FLAKE_GENESIS_NONCE;
 
   namespace testnet
   {
---
+++ src/flakechain.h
@@ -0,0 +1,30 @@
+//
+// Created by 0xA01 on 7/31/18.
+//
+#pragma once
+
+#define FLAKE_TRANSACTION_VERSION                       (3)
+#define FLAKE_BLOCK_MINOR_VERSION                       (7)
+#define FLAKE_BLOCK_MAJOR_VERSION                       (8)
+
+#define FLAKE_MONEY_SUPPLY                              (10000000000000000000ull)
+#define FLAKE_FINAL_SUBSIDY_PER_MINUTE                  (300000000000ull)
+
+#define P2P_SUPPORT_FLAG_TROUBLESOME_BLOCKS             0x02
+
+#define FLAKE_NAME                                      ("flakechain")
+#define FLAKE_TRUSTED_PUBKEY                            ("0000000000000000000000000000000000000000000000000000000000000000")
+
+#define FLAKE_PUBLIC_ADDRESS_BASE58_PREFIX              (151)
+#define FLAKE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX   (30614)
+#define FLAKE_PUBLIC_SUBADDRESS_BASE58_PREFIX           (31382)
+
+#define FLAKE_P2P_DEFAULT_PORT                          (12560)
+#define FLAKE_RPC_DEFAULT_PORT                          (12561)
+#define FLAKE_ZMQ_RPC_DEFAULT_PORT                      (12562)
+
+// snowflakenetwork
+#define FLAKE_NETWORK_ID                                ({0x72, 0x6e, 0x6f, 0x77, 0x66, 0x6c, 0x61, 0x6b, 0x65, 0x6e, 0x65, 0x74, 0x77, 0x6f, 0x72, 0x6b})
+#define FLAKE_GENESIS_TX                                ("010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101fbbddd38f518e908093781c1fd42242e426e77aa7fec4665e6dec8c7e3638fa8")
+// as we started in April 2018
+#define FLAKE_GENESIS_NONCE                             (2018)
