d4e9dda77d20411ce43ff36b009930439998c429
0xA01 <cryptoape@icloud.com>
2018-08-01 04:37:50 +0300

    Ubuntu deploy requirements

---
+++ bin/requirements-ubuntu.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install --yes \
+    git-core \
+    build-essential \
+    cmake \
+    libboost-all-dev \
+    miniupnpc \
+    libunbound-dev \
+    graphviz \
+    doxygen \
+    libunwind8-dev \
+    pkg-config \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    libgtest-dev \
+    libreadline-dev \
+    libminiupnpc-dev \
+    libzmq3-dev
