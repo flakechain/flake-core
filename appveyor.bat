acafcc6e04c9af5de607ddf9be0b6a025da538a9
troublesomepony <troublesomepony@gmail.com>
2018-09-04 18:52:00 +0300

    Quote GCC7 Toolchain URL

--- appveyor.bat
+++ appveyor.bat
@@ -7,7 +7,7 @@ echo MSYS64 Files
 cd C:\msys64\
 dir
 echo Patching
-curl https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/7.3.0/threads-posix/seh/x86_64-7.3.0-release-posix-seh-rt_v5-rev0.7z --output x86_64-7.3.0-release-posix-seh-rt_v5-rev0.7z
+curl "https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/7.3.0/threads-posix/seh/x86_64-7.3.0-release-posix-seh-rt_v5-rev0.7z/download"
 echo Extracting
 7z e x86_64-7.3.0-release-posix-seh-rt_v5-rev0.7z
 echo Patched files
