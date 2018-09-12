705c419069a0d7ba28fcbf9b2c0f19cd0b63bcb7
0xA01 <36167681+0xA01@users.noreply.github.com>
2018-09-12 05:14:59 +0300

    Add FIXME tag to find later

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -127,7 +127,9 @@ namespace nodetool
     virtual bool unblock_host(const epee::net_utils::network_address &address);
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
-    const std::vector<std::string> m_seed_nodes_list = {};
+    const std::vector<std::string> m_seed_nodes_list = {
+      // FIXME (seed-nodes)
+    };
 
     bool islimitup=false;
     bool islimitdown=false;
